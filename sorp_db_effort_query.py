66bb43dc6ff670e0398a247585e9f3775d3039c3
marinoalexander5 <marinoalexander5@gmail.com>
2020-04-01 11:05:11 -0300

    add effort time calculations

--- sorp_db_effort_query.py
+++ sorp_db_effort_query.py
@@ -1,2 +1,130 @@
+# TODO: Change tuples to lists and back to tuple for appending instead of creating new lists 
 import csv
 import sqlite3
+from datetime import datetime, timedelta
+import xlsxwriter as xlsw
+
+# Connect to Databse and query data
+conn = sqlite3.connect('E:/Antartida 2020/PamGuard/2020_Array_SQlite_Database _Antarctica.sqlite3')
+cur = conn.cursor()
+#data = cur.execute("SELECT Time, Visual_Effort FROM Environment ORDER BY Time ASC")
+data = cur.execute("SELECT Time, Visual_Effort FROM Environment ORDER BY Time ASC")
+time_diff = [] # Temporal calculation of closest date and time
+query_output = data.fetchall() # Time and status of every log
+status_ls = [] # Storage of all status
+effort_change = [] # Storage of datetime and status effort log where status changed  
+status_change = [] # Storage of full effort and position log status only where status changed
+total_eff = [] # Effort type and duration
+
+# Get all status
+for row in query_output:
+	status = row[1]
+	status_ls.append(status[:2]) # ON for ON, OF for OFF, IC for ICE
+
+temp_stat = 'OF' # Temporal variable to look only for changes in status
+
+# Keep only status changes
+for i, v in enumerate(status_ls):	
+	if temp_stat != v:
+		effort_change.append(query_output[i])
+		temp_stat = v
+
+# Get closest date and time from GpsData to status change and request GPS position
+for row in effort_change:
+	eff_time = row[0] # take date and time
+	if eff_time == None:
+		continue
+	eff_time_str = eff_time[:16] # take HH:MM for query filtering reference
+	eff_time_dt = datetime.strptime(eff_time, '%Y-%m-%d %H:%M:%S.%f') # convert to datetime format for operations
+	gps_times = cur.execute("SELECT GpsDate, Latitude, longitude FROM gpsData WHERE GpsDate LIKE (?)", (eff_time_str + '%',))	
+	gps_times_ls = gps_times.fetchall()
+	
+	if len(gps_times_ls) != 0:
+		for time in gps_times_ls:
+			gps_time_dt = datetime.strptime(time[0], '%Y-%m-%d %H:%M:%S.%f')
+			time_diff.append(abs(eff_time_dt - gps_time_dt))
+		indice = time_diff.index(min(time_diff))
+		full_row = row + gps_times_ls[indice]
+		status_change.append(full_row)
+		time_diff = []
+	else: 
+		full_row = row + (None,None,None)
+		status_change.append(full_row)	
+
+# Resultado final status_change
+# for row in status_change:
+# 	print(row)
+# Calculate time differences por total effort
+seconds_in_day = 24 * 60 * 60
+
+for row, items in enumerate(status_change):
+	try:	
+		if not items[1].startswith('OFF'):	
+			ini_time = datetime.strptime(items[0], '%Y-%m-%d %H:%M:%S.%f')
+			end_time = datetime.strptime(status_change[row+1][0], '%Y-%m-%d %H:%M:%S.%f')
+			time_delta = end_time-ini_time
+			total_eff.append((items[1], str(time_delta)))	
+			row_ls = list(items)
+			row_ls.append(str(time_delta))
+			status_change[row] = tuple(row_ls)
+		else:
+			row_ls = list(items)
+			row_ls.append(None)
+			status_change[row] = tuple(row_ls)
+	except:
+		print('error en: ', row, items)
+		row_ls = list(items)
+		row_ls.append(None)
+		status_change[row] = tuple(row_ls)
+
+for i in status_change:
+	print(i)
+
+## Write data to xlsx file
+
+#Create book and sheet
+workbook = xlsw.Workbook('D:/Usuario/Documentos/Cethus/antartida/Campaña-2020/Effort_query.xlsx')
+worksheet1 = workbook.add_worksheet('Esfuerzo visual completo')
+worksheet2 = workbook.add_worksheet('Calculo esfuerzo visual')
+
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': 1})
+
+# Set starting position
+row = 0
+col = 0
+
+# Write titles
+
+titles = ['Time', 'Effort Status', 'GpsDate', 'Latitude', 'Logitude', 'Effort duration']
+for title in titles:
+	worksheet2.write(row, col, title, bold)
+	col += 1
+
+# Write the data
+row = 1
+col = 0
+for Time, Effort, GpsDate, Latitude, Longitude, Effort_dur in status_change:
+	# # Change dot for comma for xcel format
+	# if Latitude or Longitude == None:
+	# 	pass
+	# else:
+	# 	lat = list(Latitude)
+	# 	lon = list(Longitude)
+	# 	lat[3] = ','
+	# 	lon[3] = ','
+	# 	Latitude = "".join(lat)
+	# 	Longitude = "".join(lon)
+
+	# Format columns and write the data
+	worksheet2.write(row, col, Time)
+	worksheet2.write(row, col+1, Effort)
+	worksheet2.write(row, col+2, GpsDate)
+	worksheet2.write(row, col+3, Latitude)
+	worksheet2.write(row, col+4, Longitude)
+	worksheet2.write(row, col+5, Effort_dur)
+
+	row += 1
+
+workbook.close()
+conn.close()
--- sorp_db_sightings_query.py
+++ sorp_db_sightings_query.py
@@ -1,4 +1,5 @@
-import csv
+# TODO: Change tuples to lists and back to tuple for appending instead of creating new lists 
+import xlsxwriter as xlsw
 import sqlite3
 from datetime import datetime
 # import gpxpy
@@ -6,14 +7,17 @@ from datetime import datetime
 
 conn = sqlite3.connect('E:/Antartida 2020/PamGuard/2020_Array_SQlite_Database _Antarctica.sqlite3')
 cur = conn.cursor()
-# data = cur.execute("SELECT Sighting_No,Sighting_Time_GMT__,Visual_Species,Min_Group_Size,Max_Group_Size,Best_Group_Size,Distance,Distance_Units,Angle_Board,Detector FROM Sightings")
-data = cur.execute("SELECT Sighting_No, Sighting_Time_GMT__, Visual_Species, Sighting_effort FROM Sightings WHERE Sighting_effort LIKE 'ON%' ORDER BY Sighting_Time_GMT__ ASC")
+data = cur.execute("SELECT Sighting_No,Sighting_Time_GMT__,Visual_Species,Min_Group_Size,Max_Group_Size,Best_Group_Size, Sighting_effort, Distance, Distance_Units, Photograph, Photographer, Comments FROM Sightings ORDER BY Sighting_Time_GMT__ ASC")
+#data = cur.execute("SELECT Sighting_No,Sighting_Time_GMT__,Visual_Species,Min_Group_Size,Max_Group_Size,Best_Group_Size,Distance,Distance_Units,Angle_Board,Detector FROM Sightings ORDER BY Sighting_Time_GMT__ ASC")
+#data = cur.execute("SELECT Sighting_No, Sighting_Time_GMT__, Visual_Species, Sighting_effort FROM Sightings WHERE Sighting_effort LIKE 'ON%' ORDER BY Sighting_Time_GMT__ ASC")
 time_diff = []
 results = data.fetchall()
 lista = []
 
 for row in results:
 	sight_time = row[1] # take date and time
+	if sight_time == None:
+		continue
 	sight_time_str = sight_time[:16] # take HH:MM for query filtering reference
 	sight_time_dt = datetime.strptime(sight_time, '%Y-%m-%d %H:%M:%S.%f') # convert to datetime format for operations
 	gps_times = cur.execute("SELECT GpsDate, Latitude, longitude FROM gpsData WHERE GpsDate LIKE (?)", (sight_time_str + '%',))	
@@ -31,12 +35,66 @@ for row in results:
 		row_full = row + (None,None,None)
 		lista.append(row_full)
 
-with open('D:/Usuario/Documentos/Cethus/antartida/Campaña-2020/python-sqlite3-queries-antarctica/query_output.csv', 'w', newline='') as f:
-	writer = csv.writer(f)
-	writer.writerow(['Sighting_No', 'Time', 'Species', 'Effort', 'GpsDate', 'Latitude', 'Logitude'])
-	writer.writerows(lista)
-	
+## Write data to xlsx file
 
+#Create book and sheet
+workbook = xlsw.Workbook('D:/Usuario/Documentos/Cethus/antartida/Campaña-2020/Sightings_query.xlsx')
+worksheet = workbook.add_worksheet()
+
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': 1})
+
+# Set starting position
+row = 0
+col = 0
+
+# Write titles
+
+titles = ['Sighting_No', 'Time', 'Species', 'Min', 'Max', 'Best', 'Effort', 'Distance', 'Distance_Units', 'Photograph', 'Photographer', 'Comments', 'GpsDate', 'Latitude', 'Logitude']
+for title in titles:
+	worksheet.write(row, col, title, bold)
+	col += 1
+
+# Write the data
+row = 1
+col = 0
+for Sighting_No, Time, Species, Min, Max, Best, Effort, Distance, Distance_Units, Photograph, Photographer, Comments, GpsDate, Latitude, Logitude in lista:
+	# # Change dot for comma for xcel format
+	# if Latitude or Longitude == None:
+	# 	pass
+	# else:
+	# 	lat = list(Latitude)
+	# 	lon = list(Longitude)
+	# 	lat[3] = ','
+	# 	lon[3] = ','
+	# 	Latitude = "".join(lat)
+	# 	Longitude = "".join(lon)
+
+	# Format columns and write the data
+	worksheet.write_string(row, col, Sighting_No)
+	worksheet.write(row, col+1, Time)
+	worksheet.write_string(row, col+2, Species)
+	worksheet.write_number(row, col+3, Min)
+	worksheet.write_number(row, col+4, Max)
+	worksheet.write_number(row, col+5, Best)
+	worksheet.write_string(row, col+6, Effort)
+	worksheet.write(row, col+7, Distance)
+	worksheet.write(row, col+8, Distance_Units)
+	worksheet.write(row, col+9, Photograph)
+	worksheet.write(row, col+10, Photographer)
+	worksheet.write(row, col+11, Comments)
+	worksheet.write(row, col+12, GpsDate)
+	worksheet.write(row, col+13, Latitude)
+	worksheet.write(row, col+14, Logitude)
+	row += 1
+
+workbook.close()
+conn.close()
+##################################################################################
+# with open('D:/Usuario/Documentos/Cethus/antartida/Campaña-2020/python-sqlite3-queries-antarctica/Sightings_query.xlsx', 'w', newline='') as f:
+# 	writer = csv.writer(f)
+# 	writer.writerow(['Sighting_No', 'Time', 'Species', 'Min', 'Max', 'Best', 'Effort', 'GpsDate', 'Latitude', 'Logitude'])
+# 	writer.writerows(lista)
 
 ###################################################################################
 # gpx_file = open('E:/Antartida 2020/Tracks Garmin (inclompleto)/Track_ 020-01-07 200732.gpx', 'r')
